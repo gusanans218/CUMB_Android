0b1f0ef46201aa8f3be1f0aae4bb60157067033a
SeungHyun <s20035@gsm.hs.kr>
2022-06-29 19:53:16 +0900

    Dialog 추가

--- app/src/main/java/com/example/cumb_android/GameActivity.kt
+++ app/src/main/java/com/example/cumb_android/GameActivity.kt
@@ -1,15 +1,29 @@
 package com.example.cumb_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.example.cumb_android.databinding.ActivityGameBinding
+import com.example.cumb_android.databinding.ActivityMainBinding
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        val binding: ActivityGameBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_game)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.cancel)
+
+
+        binding.gameInformation.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.show()
+        }
     }
 }
